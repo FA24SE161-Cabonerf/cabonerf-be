42c8f595c2d485c5239f2e9b49b27a839a8edbfc
phhs <phanson03vt@gmail.com>
2024-09-14 23:52:26 +0700

    fix mapstruct

--- src/main/java/com/example/caboneftbe/dto/UserDto.java
+++ src/main/java/com/example/caboneftbe/dto/UserDto.java
@@ -1,7 +1,13 @@
 package com.example.caboneftbe.dto;
 
 import com.example.caboneftbe.models.Role;
+import lombok.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
 public class UserDto {
     long id;
     String userName;
--- src/main/java/com/example/caboneftbe/request/LoginByEmailRequest.java
+++ src/main/java/com/example/caboneftbe/request/LoginByEmailRequest.java
@@ -2,15 +2,15 @@ package com.example.caboneftbe.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginByEmailRequest {
     @Email
     String email;
