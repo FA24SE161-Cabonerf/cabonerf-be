6cf78f348727133d8efaad99209334fcf08204b0
thangdmse161126 <thangdmse161126@fpt.edu.vn>
2024-09-20 21:44:13 +0700

    refresh token when verify

--- src/main/java/com/example/caboneftbe/repositories/RefreshTokenRepository.java
+++ src/main/java/com/example/caboneftbe/repositories/RefreshTokenRepository.java
@@ -2,9 +2,13 @@ package com.example.caboneftbe.repositories;
 
 import com.example.caboneftbe.models.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+    @Query("select t from RefreshToken t where t.isValid = true and t.users.id = ?1 order by t.createdAt desc")
+    Optional<RefreshToken> findTopByTokenOrderByCreatedAtDesc(long userId);
+
 }
