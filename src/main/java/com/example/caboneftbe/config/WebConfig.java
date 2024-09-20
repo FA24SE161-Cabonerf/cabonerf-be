e8deadedd66f73f2f7f79fa7f88c01d2ba9f7468
thangdmse161126 <thangdmse161126@fpt.edu.vn>
2024-09-20 14:33:59 +0700

    response token

--- src/main/java/com/example/caboneftbe/config/WebConfig.java
+++ src/main/java/com/example/caboneftbe/config/WebConfig.java
@@ -10,6 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH");
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
+                .allowedHeaders("*");
     }
 }
