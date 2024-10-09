cf31afc796be036c93a54dadb0ffa1638039088b
thangdmse161126 <thangdmse161126@fpt.edu.vn>
2024-10-09 15:44:12 +0700

    disable email service

--- src/main/java/com/example/caboneftbe/services/EmailService.java
+++ src/main/java/com/example/caboneftbe/services/EmailService.java
@@ -1,6 +1,6 @@
 package com.example.caboneftbe.services;
 
 public interface EmailService {
-    void sendVerifyRegisterEmail(String toAddress, String emailVerifyToken);
-    void sendForgotPasswordEmail(String toAddress, String forgotPasswordToken);
+//    void sendVerifyRegisterEmail(String toAddress, String emailVerifyToken);
+//    void sendForgotPasswordEmail(String toAddress, String forgotPasswordToken);
 }
