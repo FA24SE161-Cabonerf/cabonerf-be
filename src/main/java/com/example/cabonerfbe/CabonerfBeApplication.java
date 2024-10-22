1687f39a0738f26c9b8b8b403196d7a2add64785
phhs <phanson03vt@gmail.com>
2024-10-22 13:45:29 +0700

    [CABONERF-101][Son]: enable jpa auditing for auto update createdAt, modifiedAt.

--- src/main/java/com/example/cabonerfbe/CabonerfBeApplication.java
+++ src/main/java/com/example/cabonerfbe/CabonerfBeApplication.java
@@ -2,8 +2,10 @@ package com.example.cabonerfbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CabonerfBeApplication {
 
     public static void main(String[] args) {
