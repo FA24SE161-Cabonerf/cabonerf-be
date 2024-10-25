accb5295a7ff89070e9b57e591a0e54e4702fb64
thangdmse161126 <thangdmse161126@fpt.edu.vn>
2024-10-25 19:07:17 +0700

    :art: [Thang] validate response get project by id

--- src/main/java/com/example/cabonerfbe/converter/ConnectorConverter.java
+++ src/main/java/com/example/cabonerfbe/converter/ConnectorConverter.java
@@ -3,6 +3,7 @@ package com.example.cabonerfbe.converter;
 import com.example.cabonerfbe.dto.ConnectorDto;
 import com.example.cabonerfbe.models.Connector;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -11,6 +12,15 @@ import java.util.List;
 public interface ConnectorConverter {
     ConnectorConverter INSTANCE = Mappers.getMapper(ConnectorConverter.class);
 
-    ConnectorDto fromConnectorToConnectorDto(Connector connector);
+    @Mapping(source = "startProcess.id",target = "startProcessId")
+    @Mapping(source = "endProcess.id",target = "endProcessId")
+    @Mapping(source = "startExchanges.id",target = "startExchangesId")
+    @Mapping(source = "endExchanges.id",target = "endExchangesId")
     List<ConnectorDto> fromListConnectorToConnectorDto(List<Connector> connector);
+
+    @Mapping(source = "startProcess.id",target = "startProcessId")
+    @Mapping(source = "endProcess.id",target = "endProcessId")
+    @Mapping(source = "startExchanges.id",target = "startExchangesId")
+    @Mapping(source = "endExchanges.id",target = "endExchangesId")
+    ConnectorDto fromConnectorToConnectorDto(Connector connector);
 }
