885523fc6e8e7b77d5a776f39a3aa4f37dbe5368
thangdmse161126 <thangdmse161126@fpt.edu.vn>
2024-12-01 14:42:58 +0700

    :recycle:[Thang] add LifeCycleBreakdown

--- src/main/java/com/example/cabonerfbe/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/example/cabonerfbe/services/impl/ProjectServiceImpl.java
@@ -154,8 +154,8 @@ public class ProjectServiceImpl implements ProjectService {
 
         Pageable pageable = PageRequest.of(pageCurrent - PAGE_INDEX_ADJUSTMENT, pageSize);
 
-//        UserOrganization uo = uoRepository.findByUserAndOrganization(organizationId, userId)
-//                .orElseThrow(() -> CustomExceptions.unauthorized(MessageConstants.USER_NOT_BELONG_TO_ORGANIZATION));
+        UserOrganization uo = uoRepository.findByUserAndOrganization(organizationId, userId)
+                .orElseThrow(() -> CustomExceptions.unauthorized(MessageConstants.USER_NOT_BELONG_TO_ORGANIZATION));
 
         Page<Project> projects;
         if (methodId == null) {
