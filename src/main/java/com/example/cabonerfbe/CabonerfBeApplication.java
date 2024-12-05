cb28bb123c3970168b803c4a1b215056612532ce
phhs <phanson03vt@gmail.com>
2024-12-05 11:55:40 +0700

    [Son]: attempt to fix exchange calculation

--- src/main/java/com/example/cabonerfbe/CabonerfBeApplication.java
+++ src/main/java/com/example/cabonerfbe/CabonerfBeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(scanBasePackages = "com.example.cabonerfbe")
+@SpringBootApplication
 @EnableJpaAuditing
 public class CabonerfBeApplication {
 
